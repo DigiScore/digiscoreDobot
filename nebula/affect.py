f6c11d7e5d4ca57be730c0a68feb813500d6381f
craigv <vear2ev2.co.uk>
2022-09-07 10:41:36 +0100

    AIFactory working - but very hacky - P to work with new net designs

---
+++ nebula/affect.py
@@ -0,0 +1,10 @@
+class Affect:
+
+    def __init__(self):
+        # names for affect listening
+        self.affectnames = ['user_in',
+                            'rnd_poetry',
+                            'affect_net',
+                            'self_awareness']
+
+        self.affect_logging = False
--- nebula/new_nebula.py
+++ nebula/new_nebula.py
@@ -1,122 +1,88 @@
+"""
+Embodied AI Engine Prototype AKA "Nebula".
+This object takes a live signal (such as body tracking,
+or real-time sound analysis) and generates a response that
+aims to be felt as co-creative. The response is a flow of
+neural network emissions data packaged as a dictionary,
+and is gestural over time. This, when plugged into a responding
+script (such as a sound generator, or QT graphics) gives
+the impression of the AI creating in-the-moment with the
+human in-the-loop.
+
+© Craig Vear 2022
+cvear@dmu.ac.uk
+
+Dedicated to Fabrizio Poltronieri
+"""
+# import python modules
 from dataclasses import dataclass, fields
-from random import random
+from random import random, randrange
 from threading import Thread
-# import tensorflow as tf
+import tensorflow as tf
+import numpy as np
+from time import sleep
 
+# import Nebula modules
+from ai_factory import AIFactory
+from affect import Affect
+from nebula_dataclass import NebulaDataClass
 
-class NebulaDataEngine:
-    """An AI engine that generates gestural thought trains.
-        This is the soul of embodied musicking.
+class Nebula:
+    """Nebula is the core "director" of an AI factory.
+     It generates data in response to incoming percpts
+    from human-in-the-loop interactions, and responds
+    in-the-moment to the gestural input of live data.
+    There are 4 components:
+        Nebula: as "director" it coordinates the overall
+            operations of the AI Factory
+        AIFactory: builds the neural nets that form the
+            factory, coordinates data exchange,
+            and liases with the common data dict
+        NebulaDataClass: is the central dataclass that
+            holds and shares all the  data exchanges
+            in the AI factory
+        Affect: receives the live percept input from
+            the client and produces an affectual response
+            to it's energy input, which in turn interferes
+            with the data generation.
 
-        args:
-            speed: general tempo/ feel of Nebula's response (0.5 ~ moderate fast, 1 ~ moderato; 2 ~ presto)"""
+    Args:
+        speed: general tempo/ feel of Nebula's response (0.5 ~ moderate fast, 1 ~ moderato; 2 ~ presto)"""
 
     def __init__(self, speed=1):
         print('building engine server')
 
-        # todo - global speed should be linked to bpm
         # Set global vars
         self.interrupt_bang = False
-        self.global_speed = speed  # / 10
         self.rnd_stream = 0
         self.rhythm_rate = 1
         self.affect_listen = 0
 
         # logging on/off switches
-        self.net_logging = False
         self.master_logging = False
-        self.streaming_logging = False
-        self.affect_logging = False
 
         # build the dataclass and fill with random number
         self.datadict = NebulaDataClass()
         print(f'Data dict initial values are = {self.datadict}')
 
-        # Build the AI factory
-        # self.AI_factory_build()
+        # Build the AI factory and pass it the data dict
+        self.AI_factory = AIFactory(self.datadict, speed)
+
+    def director(self):
+        """Starts the threads that gets the data rolling."""
+        # declares all threads
+        t1 = Thread(target=self.AI_factory.make_data)
+        t2 = Thread(target=self.Affect.affect)
+
+        # assigns them a daemon
+        t1.daemon = True
+
+        # start them all
+        t1.start()
+        t2.start()
 
 
-
-    # todo - build as a class where user only inputs the list of nets required
-    def AI_factory_build(self):
-        """Builds the individual neural nets that constitute the AI factory.
-        This will need modifying if and when a new AI factory design is implemented.
-        NB - the list of netnames will also need updating"""
-
-        # instantiate nets as objects and make  models
-        print('MoveRNN initialization')
-        self.move_net = tf.keras.models.load_model('nebula/models/EMR-full-sept-2021_RNN_skeleton_data.nose.x.h5')
-        print('AffectRNN initialization')
-        self.affect_net = tf.keras.models.load_model('nebula/models/EMR-full-sept-2021_RNN_bitalino.h5')
-        print('MoveAffectCONV2 initialization')
-        self.move_affect_net = tf.keras.models.load_model('nebula/models/EMR-full-sept-2021_conv2D_move-affect.h5')
-        print('AffectMoveCONV2 initialization')
-        self.affect_move_net = tf.keras.models.load_model('nebula/models/EMR-full-sept-2021_conv2D_affect-move.h5')
-        print('MoveAffectCONV2 initialization')
-        self.affect_perception = tf.keras.models.load_model('nebula/models/EMR-full-sept-2021_conv2D_move-affect.h5')
-
-        # name list for nets that align to factory above
-        self.netnames = ['move_rnn',
-                         'affect_rnn',
-                         'move_affect_conv2',
-                         'affect_move_conv2',
-                         'self_awareness',  # Net name for self-awareness
-                         'master_output']  # input for self-awareness
-
-    def random_dict_fill(self):
-        """Fills the working dataclass with random values"""
-        for field in fields(self.datadict):
-            # print(field.name)
-            rnd = random()
-            setattr(self.datadict, field.name, rnd)
-        print(f'Data dict new random values are = {self.datadict}')
-
-
-class Affect:
-
-    def __init__(self):
-        # names for affect listening
-        self.affectnames = ['user_in',
-                            'rnd_poetry',
-                            'affect_net',
-                            'self_awareness']
-
-
-@dataclass
-class NebulaDataClass:
-    """Dataclass containing all the data emissions
-    and user input to and from Nebula"""
-
-    move_rnn: float = random()
-    """Net 1 raw emission"""
-
-    affect_rnn: float = random()
-    """Net 2 raw emission"""
-
-    move_affect_conv2: float = random()
-    """Net 3 raw emission"""
-
-    affect_move_conv2: float = random()
-    """Net 4 raw emission"""
-
-    master_output: float = random()
-    """Master output from the affect process"""
-
-    user_in: float = random()
-    """Percept input stream from client e.g. live mic level"""
-
-    rnd_poetry: float = random()
-    """Random stream to spice things up"""
-
-    # affect_net: float = random()
-    # """Net"""
-
-    self_awareness: float = random()
-    """Net that has some self awareness - ???"""
-
-    affect_decision: float = random()
-    """Current stream chosen by affect process"""
-
-    rhythm_rate: float = 0.1
-    """Internal clock/ rhythm sub division"""
+if __name__ == '__main':
+    test = Nebula()
+    test.director()
 
